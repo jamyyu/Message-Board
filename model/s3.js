6fbaf9e5484e901d87b837842de3184413dd3ac5
jamyyu <jamy11212@gmail.com>
2024-07-27 23:21:10 +0800

    Modify Dockerfile

--- model/s3.js
+++ model/s3.js
@@ -1,9 +1,9 @@
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 //import { getSignedUrl } from "@aws-sdk/s3-request-presigner"; 
-import { resizeImage } from './resizeImg.js';
+//import { resizeImage } from './resizeImg.js';
 import dotenv from 'dotenv';
 import crypto from 'crypto';
-import sharp from 'sharp';
+//import sharp from 'sharp';
 
 dotenv.config();
 
