ed1e08c2c44f6b7d35983208d39ae6c19f2a4014
jamyyu <jamy11212@gmail.com>
2024-07-27 21:11:02 +0800

    Modify resizeImg.js

--- model/resizeImg.js
+++ model/resizeImg.js
@@ -5,8 +5,7 @@ export const resizeImage = async(fileBuffer) => {
     const image = await Jimp.read(fileBuffer);
     const buffer = await image
       .resize(200, Jimp.AUTO) // 調整寬度到 200，保持比例
-      .contain(200, Jimp.AUTO) // 適應指定大小，保持比例
-      .getBufferAsync(Jimp.MIME_PNG); // 轉換為 PNG 格式的緩衝區
+      .getBufferAsync(Jimp.AUTO); // 轉換為 PNG 格式的緩衝區
     return buffer;
   } catch (error) {
     console.error('Error processing image:', error);
